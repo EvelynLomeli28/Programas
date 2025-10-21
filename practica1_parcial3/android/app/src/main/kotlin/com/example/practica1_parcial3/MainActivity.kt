cecbef6aacfcf98aebd2c23367f5524b87fc36ef
EvelynLomeli28 <evelynlomeli232@gmail.com>
2025-10-21 08:29:14 -0600

    no message

---
+++ practica1_parcial3/android/app/src/main/kotlin/com/example/practica1_parcial3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practica1_parcial3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ practica1_parcial3/lib/widget/pagina2.dart
@@ -0,0 +1,19 @@
+ import 'package:flutter/material.dart';
+
+class pagina2 extends StatefulWidget{
+  const pagina2({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return Datos();
+  }
+
+}
+class Datos extends State<pagina2>{
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+
+  );
+  }
+}
