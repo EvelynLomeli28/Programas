ff46278bd6e951c465a683b9cb30a80af2bc376a
EvelynLomeli28 <evelynlomeli232@gmail.com>
2025-10-28 07:26:50 -0600

    no message

---
+++ practica2_parcialll/android/app/src/main/kotlin/com/example/practica2_parcialll/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practica2_parcialll
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ practica2_parcialll/lib/widget/pagina1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Pagina1 extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return Disenio();
+  }
+}
+class Disenio extends State<Pagina1>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
