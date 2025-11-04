821c2037bb20f7b1dff5275dfd3870ac95c29a87
EvelynLomeli28 <evelynlomeli232@gmail.com>
2025-11-04 11:02:28 -0600

    no message

---
+++ app5_parcial3/lib/widget/detalles.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class Detalles extends StatefulWidget{
+  final String clave;
+  final String nombre;
+  final String correo;
+  const Detalles({
+    super.key,
+    required this.clave,
+    required this.nombre,
+    required this.correo
+    });
+
+  @override
+  State<StatefulWidget> createState() {
+    return Clase();
+  }
+}
+class Clase extends State<Detalles>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detalles'),
+        backgroundColor: const Color.fromARGB(255, 183, 58, 93),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Card(
+            elevation: 18,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 190,
+                    height: 190,
+                    child: CircleAvatar(
+                      radius: 90,
+                      backgroundImage: NetworkImage('https://i.pinimg.com/236x/05/2c/9d/052c9da2359097380765b6e792231e30.jpg'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    child: 
+                    Text('${widget.clave}',style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 112, 11, 44)
+                    ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    child: 
+                    Text('${widget.nombre}',style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 14, 99, 79)
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    child: 
+                    Text('${widget.correo}',style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 8, 45, 71)
+                  ),
+                  ),
+                  ),
+                  
+                ],
+              ),
+            ),
+          ),
+          ),
+      ),
+    );
+  }
+
+}
--- app5_parcial3/lib/widget/pagina1.dart
+++ app5_parcial3/lib/widget/pagina1.dart
@@ -1,92 +1,133 @@
 
-import 'dart:nativewrappers/_internal/vm/lib/math_patch.dart';
+import 'package:app5_parcial3/widget/detalles.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+class Lista extends StatefulWidget{
+  const Lista({super.key});
 
-class Pagina1 extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
-    return datos();
+    return Clase();
   }
 }
-class datos extends State<Pagina1>{
+class Item{
+  final String clave;
+  final String nombre;
+  final String correo;
+  Item(this.clave, this.nombre,this.correo);
+}
+class Clase extends State<Lista>{
+  TextEditingController nombre = TextEditingController();
+  TextEditingController correo = TextEditingController();
+List<Item> items =[];
 
-  void _agregar(){
-    setState(() {
-      final String n = nombre.text;
-      final String c = correo.text;
-      int numero = 0;   
-      }
+void _agregar(){
+   setState(() {
+     final String n = nombre.text;
+     final String c = correo.text;
+     int numero = Random().nextInt(100);
+     if(n.isNotEmpty && c.isNotEmpty){
+        final String clave = numero.toString() + n.substring(0,2).toUpperCase();
+        print(clave);
+        items.add(Item('$clave','$n','$c'));
+        nombre.clear();
+        correo.clear();
+     }else{
+       ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Faltan datos")),
       );
-  }
-  final TextEditingController nombre = TextEditingController();
-  final TextEditingController correo = TextEditingController();
+     }
+   });
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Listas",
+        title: Text('Listas',
         style: TextStyle(
-          color: Colors.white
-        ),),
-        backgroundColor: const Color.fromARGB(255, 183, 58, 96),
+          color: Colors.white,
+        ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 183, 58, 98),
       ),
       body: Center(
-        child: Padding(padding: EdgeInsetsGeometry.all(15),
-        child: Card(
-          elevation: 17,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: nombre,
-                  decoration: InputDecoration(
-                    labelText: "Nombre",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                TextField(
-                  controller: correo,
-                  decoration: InputDecoration(
-                    labelText: "Correo",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                ElevatedButton(onPressed: (){}, 
-                child: Text("Agregar")),
-                Expanded(child: ListView.builder(
-                  itemCount: 3,
-                  itemBuilder: (context, index) =>Card(
-                    child: ListTile(
-                      leading: Icon(Icons.account_box),
-                      title: Text(""),
-                      subtitle: Column(
-                        children: [
-                          Text("",
-                          style: TextStyle(
-                            fontFamily: 'Verdana',
-                            fontSize: 20,
-                          ),),
-                          Text("data"),
-                        ],
-                      ),
-                      trailing: Icon(Icons.forward_outlined),
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Card(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: nombre,
+                    decoration: InputDecoration(
+                      labelText: 'Nombre',
+                      border: OutlineInputBorder(),
                     ),
-                  ) 
-
-                ),
-                ),
-              ],
+                  ),
+                  TextField(
+                    controller: correo,
+                    decoration: InputDecoration(
+                      labelText: 'Correo',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      onPressed: _agregar, 
+                      label: Text('Agregar')
+                      ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (context, index) => Card(
+                        child: ListTile(
+                          leading: Icon(Icons.person),
+                          title: Text(items[index].clave),
+                          subtitle: Column(
+                            children: [
+                              Text(items[index].nombre,style: TextStyle(
+                                fontFamily: "Arial",
+                                fontSize: 20,
+                              ),),
+                              Text(items[index].correo),
+                            ],
+                          ),
+                          trailing: Icon(Icons.arrow_forward),
+                          onTap: () {
+                            print(items[index].clave+items[index].nombre+items[index].correo);
+                            Navigator.push(
+                              context, MaterialPageRoute(
+                                builder: (context) => Detalles(
+                                  clave : items[index].clave,
+                                  nombre : items[index].nombre,
+                                  correo : items[index].correo,
+                                ),
+                              ),
+                              );
+                          },
+                        ),
+                      ),
+                      ),
+                    ),  
+                ],
+              ),
             ),
           ),
-        ),
-        ),
+          ),
       ),
     );
   }
+
 }
