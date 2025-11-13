935c49d2dc5249a1da62c9dce7fd58abf959962d
EvelynLomeli28 <evelynlomeli232@gmail.com>
2025-11-13 07:57:31 -0600

    no message

--- app1_unidad4/lib/base de datos/basedhelper.dart
+++ app1_unidad4/lib/base de datos/basedhelper.dart
@@ -2,40 +2,44 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class Basedatoshelper {
-  static final Basedatoshelper _instance = Basedatoshelper._internal();
-  factory Basedatoshelper() => _instance;
-  Basedatoshelper._internal();
+   static final Basedatoshelper _instance = Basedatoshelper._internal();
+   factory Basedatoshelper() => _instance;
 
-  Database? _database;
+   Basedatoshelper._internal();
 
-  Future<Database> get database async {
+   Database? _database;
+
+   Future<Database> get database async{
     if (_database != null) return _database!;
     _database = await _initDB();
     return _database!;
-  }
 
-  Future<Database> _initDB() async {
+   }
+
+   Future <Database> _initDB() async{
     String path = join(await getDatabasesPath(), 'usuarios.db');
     return await openDatabase(
       path,
       version: 1,
-      onCreate: (db, version) async {
+      onCreate: (db, version) async{
         await db.execute('''
-                CREATE TABLE usuarios(
-                id INTEGER PRIMARY KEY AUTOINCREMENTE,
-                usuario TEXT NOT NULL,
-                password TEXT NOT NULL,
-                )
-                ''');
-      },
+            CREATE TABLE usuarios(
+             id INTEGER PRIMARY KEY AUTOINCREMENTE,
+             usuario TEXT NOT NULL,
+             password TEXT NOT NULL,
+            )
+          ''');
+      }
     );
-  }
+   }
 
-  Future<int> insertar(String usuario, String password) async {
-    final db = await database;
-    return await db.insert('usuarios', {
-      'usuario': usuario,
-      'password': password,
-    });
-  }
+    Future<int> insertar(String usuario, String password) async{
+      final db = await database;
+      return await db.insert('usuarios',{
+        'usuario' : usuario,
+        'password' : password,
+      });
+    }
+
+   
 }
--- app1_unidad4/lib/widget/Pagina1.dart
+++ app1_unidad4/lib/widget/Pagina1.dart
@@ -1,46 +1,75 @@
 import 'package:flutter/material.dart';
-
-class Pagina extends StatefulWidget{
+class Pagina1 extends StatefulWidget{
+  const Pagina1({super.key});
   @override
   State<StatefulWidget> createState() {
-    return Disenio();
+    return Clase();
   }
 }
-class Disenio extends State<Pagina>{
+
+class Clase extends State<Pagina1>{
+  final TextEditingController usuario = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  void _agregar(){
+    String u = usuario.text;
+    String p = password.text;
+    if(u.isNotEmpty && p.isNotEmpty){
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Practica 1 Unidad 4'),
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: Center(
-        child: Padding(padding: EdgeInsetsGeometry.all(10),
+   return Scaffold(
+    appBar: AppBar(
+      title: Text('Pagina 1'),
+      backgroundColor: const Color.fromARGB(255, 255, 65, 141),
+    ),
+    body: Center(
+      child: Padding(
+        padding: EdgeInsets.all(20),
         child: Card(
-          elevation: 18,
+          elevation: 10,
           shape: BeveledRectangleBorder(
-            borderRadius: BorderRadius.circular(10)
-          ),
+            borderRadius: BorderRadius.circular(8),
+            ),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(15.0),
             child: Column(
-              mainAxisSize:MainAxisSize.min,
-                children: [
+              mainAxisSize: MainAxisSize.min,
+              children: [
                   TextField(
+                    controller: usuario,
                     decoration: InputDecoration(
-                      label: Text('Isuario')
+                      labelText: 'Escribe el usuario',
+                      border: OutlineInputBorder(),
+                       prefixIcon: Icon(Icons.person),
+                       prefixIconColor: const Color.fromARGB(255, 183, 58, 81)
                     ),
                   ),
                   TextField(
+                    controller: password,
                     decoration: InputDecoration(
-                      label: Text('Password')
+                      labelText: 'Escribe el password',
+                      border: OutlineInputBorder(),
+                       prefixIcon: Icon(Icons.password),
+                       prefixIconColor: const Color.fromARGB(255, 183, 58, 108)
                     ),
+                  ),
+                  SizedBox(height: 15),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      onPressed: _agregar, 
+                      label: Text('Agregar'),
+                      icon: Icon(Icons.insert_chart),
+                      ),
                   )
-                ],
-            ),
+              ],
+               ),
           ),
-        ),),
-      ),
-    );
+          ),
+        ),
+        ),
+   );
   }
+
 }
